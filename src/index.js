a47987476e3c8f9e1b6136ae1e880b382d704e95
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-05-06 18:50:55 +0100

    Added moment() .tz to the function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-function showSelectedCountry(event) {
-  if (event.target.value !== " ") {
+function showSelectedCity(event) {
+  if (event.target.value) {
+    console.log(event.target.value);
     if (event.target.value === "paris") {
-      alert("This is Paris");
-    }
-    if (event.target.value === "toyko") {
-      alert("This is Toyko");
-    }
-    if (event.target.value === "sydney") {
-      alert("This is Sydney");
+      let parisTime = moment()
+        .tz("Europe/Paris")
+        .format("dddd, MMMM D YYYY HH:mm A");
+      alert(`It is ${parisTime} in ${event.target.value}.`);
+    } else if (event.target.value === "toyko") {
+      let tokyoTime = moment()
+        .tz("Asia/Toyko")
+        .format("dddd, MMMM D YYYY HH:mm A");
+      alert(`It is ${tokyoTime} in ${event.target.value}.`);
+    } else if (event.target.value === "sydney") {
+      let sydneyTime = moment()
+        .tz("Australia/Sydney")
+        .format("dddd, MMMM D YYYY HH:mm A");
+      alert(`It is ${sydneyTime} in ${event.target.value}.`);
     }
   }
 }
 
-let countrySelect = document.querySelector("#timezone");
-countrySelect.addEventListener("change", showSelectedCountry);
+let citySelect = document.querySelector("#timezone");
+citySelect.addEventListener("change", showSelectedCity);
